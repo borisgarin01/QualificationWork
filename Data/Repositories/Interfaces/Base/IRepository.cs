84850b2366bcb4d593ac969dfa8584bac90a1c76
Boris Garin <boris.garin01job@mail.ru>
2023-06-06 20:03:19 +0500

    Base repository interface added

---
+++ Data/Repositories/Interfaces/Base/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalQualification.Data.Repositories.Interfaces.Base
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task Create(T entity);
+        Task<T> GetById(long id);
+        Task Update(T entity);
+        Task Delete(T entity);
+    }
+}
