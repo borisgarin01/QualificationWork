6125b7a223d3dd12d7f9759dc168654d719d3b93
Boris Garin <boris.garin01job@mail.ru>
2023-06-06 20:03:05 +0500

    DataContext (DbContext created)

---
+++ Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using FinalQualification.Models.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalQualification.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Critic> Critics { get; set; }
+        public DbSet<CriticTextReview> CriticsTextReviews { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Gamer> Gamers { get; set; }
+        public DbSet<GamerReview> GamersReviews { get; set; }
+        public DbSet<Screenshot> Screenshots { get; set; }
+        public DbSet<Trailer> Trailers { get; set; }
+        public DbSet<CriticVideoReview> CriticsVideoReviews { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
